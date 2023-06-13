4d836c6e6858b4b43277f1bfa3af8b035cf81631
Sunil H N <sunilh.n27@gmail.com>
2023-06-14 01:25:31 +0530

    remove config from services

--- src/services/appwrite.config.js
+++ src/services/appwrite.config.js
@@ -5,14 +5,14 @@ require('dotenv').config({ path: '../../../app-auth/.envrc' })
 const client = new Client();
 
 
-// client
-//     .setEndpoint(`${process.env.APPWRITE_ENDPOINT}`)
-//     .setProject(`${process.env.APPWRITE_PROJECT_ID}`)
+client
+    .setEndpoint(`${process.env.APPWRITE_ENDPOINT}`)
+    .setProject(`${process.env.APPWRITE_PROJECT_ID}`)
 
 
-    client
-    .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject('647e348cd823816f4db7')
+    // client
+    // .setEndpoint('https://cloud.appwrite.io/v1')
+    // .setProject('647e348cd823816f4db7')
 
 export const account = new Account(client);
 
